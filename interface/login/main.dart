c868732443d79eaf97da316e72e889d5ff9c50ad
nex <nextec.exe@gmail.com>
2020-09-12 03:28:22 +0100

    Change file main.dart again

--- interface/login/main.dart
+++ interface/login/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Login of adopt'),
+      home: MyHomePage(title: 'Login of adopt change again'),
     );
   }
 }
