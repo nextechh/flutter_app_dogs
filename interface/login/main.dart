ecff521788a5d84d4a1110bdcabcc250579c86be
nex <nextec.exe@gmail.com>
2020-09-14 01:54:08 +0100

    commit to creat pr

--- interface/login/main.dart
+++ interface/login/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+//make sure the app is running portraitup
 void main() {
-  runApp(MyApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+  .then((_){
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -9,25 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Adopt me',
+      title: 'WazupDog',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        hintColor: Color(0xFFC0F0E),
+        primaryColor: Color(0xFF80E1D1),
+        canvasColor: Colors.transparent,
+        fontFamily: "Montserrat",
       ),
-      home: MyHomePage(title: 'Login of adopt change again'),
+      home: MyHomePage(title: 'Whats up dog - PR'),
     );
   }
 }
@@ -101,10 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Text('$_counter', style: Theme.of(context).textTheme.headline4,),
           ],
         ),
       ),
